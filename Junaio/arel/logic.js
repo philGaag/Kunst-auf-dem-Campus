7b644a80b74f331b1922201f09248ca1b0998805
Philip Gaag <philipgaag@yahoo.com>
2015-03-17 10:31:14 +0100

    object generierung ausgelagert und in js definiert

--- Junaio/arel/logic.js
+++ Junaio/arel/logic.js
@@ -1,64 +1,27 @@
-var artistProfile,
-	informationText,
+var artistProfileButton,
+	textItem,
+	informationButton,
 	closeButton,
-	gallery,
+	galleryButton,
 	position;
 
 arel.sceneReady(function()
 {
 	arel.Events.setListener(arel.Scene, function(type, param){trackingHandler(type, param);});
 	arel.Scene.getTrackingValues(function(trackingValues){receiveTrackingStatus(trackingValues);});	
+	initialiseObjects();
+	//																						   displayText(obj, type, params, itemToOpen){
+	arel.Events.setListener(arel.Scene.getObject("artistProfileButton"), function(obj, type, params){displayText(obj, type, params, textItem);});
 
-	// ***************************************************************************************
-	// OPTION 1 FOR CREATING OBJECTS
-	// ***************************************************************************************
-	// loremText = arel.Object.Model3D.createFromImage("loremText", "resources/lorem_text.png");
-	// loremText.setVisibility(false);
-	// loremText.setCoordinateSystemID(1);
-	// loremText.setScale(new arel.Vector3D(10.0,10.0,10.0));
-	// loremText.setTranslation(setPosition(loremText, 500, 0, 20));
-	// arel.Scene.addObject(loremText);
-	// ***************************************************************************************
+	arel.Events.setListener(arel.Scene.getObject("informationButton"), function(obj, type, params){displayText(obj, type, params, textItem);});
 
-	artistProfile = arel.Object.Model3D.createFromImage("artistProfile", "resources/Buttons/artist.png");
-	artistProfile.setVisibility(true);
-	artistProfile.setCoordinateSystemID(1);
-	artistProfile.setScale(new arel.Vector3D(1.0,1.0,1.0));
-	artistProfile.setTranslation(setPosition(artistProfile, 0, 0, 20));
-	arel.Scene.addObject(artistProfile);
-
-	// informationText = arel.Object.Model3D.createFromImage("informationText", "resources/Buttons/information.png");
-	// informationText.setVisibility(true);
-	// informationText.setCoordinateSystemID(1);
-	// informationText.setScale(new arel.Vector3D(1.0,1.0,1.0));
-	// informationText.setTranslation(setPosition(informationText, 100, 0, 20));
-	// arel.Scene.addObject(informationText);
-
-	// closeButton = arel.Object.Model3D.createFromImage("closeButton", "resources/Buttons/close.png");
-	// closeButton.setVisibility(true);
-	// closeButton.setCoordinateSystemID(1);
-	// closeButton.setScale(new arel.Vector3D(2.0,2.0,2.0));
-	// closeButton.setTranslation(setPosition(closeButton, 300, 0, 20));
-	// arel.Scene.addObject(closeButton);
-
-	// gallery = arel.Object.Model3D.createFromImage("gallery", "resources/Buttons/gallery.png");
-	// gallery.setVisibility(true);
-	// gallery.setCoordinateSystemID(1);
-	// gallery.setScale(new arel.Vector3D(1.0,1.0,1.0));
-	// gallery.setTranslation(setPosition(gallery, 200, 0, 20));
-	// arel.Scene.addObject(gallery);
-
-	// artistProfile = arel.Scene.getObject("artist_profile");	
-	// informationText = arel.Scene.getObject("information");	
-	// closeButton = arel.Scene.getObject("close");		
-	// gallery = arel.Scene.grtObject('gallery');									
-	// ***************************************************************************************
-	// 																										currentObject, type, params, open)
-	arel.Events.setListener(arel.Scene.getObject("artistProfile"), function(obj, type, params){openArtistInfo(obj, type, params, informationText);});
-	arel.Events.setListener(arel.Scene.getObject("closeButton"), function(obj, type, params){closeStuff(obj, type, params, loremText, artistProfile);});
+	arel.Events.setListener(arel.Scene.getObject("galleryButton"), function(obj, type, params){displayText(obj, type, params, textItem);});
+// 																							 closeItem(obj, type, params, itemToClose,		 itemToOpen)
+	arel.Events.setListener(arel.Scene.getObject("closeButton"), function(obj, type, params){closeItem(obj, type, params, informationButton, artistProfileButton);});
 });
 
-function setPosition(target, x, y, z){
+
+setPosition = function(target, x, y, z){
 	position = target.getTranslation();
     position.setX(position.getX() + x);
     position.setY(position.getY() + y);
@@ -66,42 +29,40 @@ function setPosition(target, x, y, z){
     return position;
 };
 
-function changeCoordinateSystemId(id){
-	artistProfile.setCoordinateSystemID(id);
-	informationText.setCoordinateSystemID(id);
-	gallery.setCoordinateSystemID(id);
+setCOS = function(id){
+	artistProfileButton.setCoordinateSystemID(id);
+	informationButton.setCoordinateSystemID(id);
+	galleryButton.setCoordinateSystemID(id);
+	textItem.setCoordinateSystemID(id);
+	closeButton.setCoordinateSystemID(id);
 };
 
-function trackingHandler(type, param)
-{
+trackingHandler = function(type, param){
 	//check if there is tracking information available
 	if(param[0] !== undefined)
 	{
 		//if the pattern is found, hide the information to hold your phone over the pattern and start the appear information on the trooper (if not done so already)
 		if(type && type == arel.Events.Scene.ONTRACKING && param[0].getState() == arel.Tracking.STATE_TRACKING)
 		{
-			
 			if(param[0].getCoordinateSystemID() == 1){
 				hideHtmlStuff();
-				changeCoordinateSystemId(1);
+				setCOS(1);
 			}
-			
 			if(param[0].getCoordinateSystemID() == 2){
 				hideHtmlStuff();
-				changeCoordinateSystemId(2);
+				setCOS(2);
 				// loremText.setTexture("resources/Text/lorem_text.png");
 			}
 			if(param[0].getCoordinateSystemID() == 3){
-				// kunst
 				hideHtmlStuff();
-				changeCoordinateSystemId(3);
+				setCOS(3);
 			}
 			if(param[0].getCoordinateSystemID() == 4){
 				hideHtmlStuff();
-				changeCoordinateSystemId(4);
+				setCOS(4);
 			}
 			if(param[0].getCoordinateSystemID() == 5){
-				changeCoordinateSystemId(5);
+				setCOS(5);
 			}
 		}
 		//if the pattern is lost tracking, show the information to hold your phone over the pattern
@@ -113,33 +74,34 @@ function trackingHandler(type, param)
 };
 
 //needed only for the start. If no valid tracking information is returned, show the "what to track" information
-function receiveTrackingStatus(trackingValues)
+receiveTrackingStatus = function(trackingValues)
 {
 	//the user is currently not holding the phone over a valid reference image -> fade in the information what to track
 	if(trackingValues[0] === undefined)
-		$('#info').fadeIn("fast");
-	
-	
+		$('#info').fadeIn("fast");	
 };
-function openArtistInfo(obj, type, params, open){
+displayText = function(obj, type, params, itemToOpen){
 	if(type && type === arel.Events.Object.ONTOUCHSTARTED){
     	obj.setVisibility(false);
-    	open.setVisibility(true);
+    	itemToOpen.setVisibility(true);
+    	setText("resources/Text/plexitext.png");
     	closeButton.setVisibility(true);
-    	closeButton.setParent(loremText);
+    	closeButton.setParent(itemToOpen);
     }
 };
-function closeStuff(obj, type, params, close, open){
+closeItem = function(obj, type, params, itemToClose, itemToOpen){
 	if(type && type === arel.Events.Object.ONTOUCHSTARTED){
 		obj.setVisibility(false);
 		close.setVisibility(false);
 		open.setVisibility(true);
 	}
 };
+setText = function (replacementText){
+	textItem.setTexture(replacementText);
+};
 changeChannel = function(){
 	arel.Scene.switchChannel(383691);
 }; 
-
 hideHtmlStuff = function(){
 	$('#info').hide();
 	$('#winning').show();
@@ -147,4 +109,58 @@ hideHtmlStuff = function(){
 showHtmlStuff = function(){
 	$('#info').show();
 	$('#winning').hide();
+};
+function initialiseObjects(){
+	// ***************************************************************************************
+	// OPTION 1 FOR CREATING OBJECTS
+	// ***************************************************************************************
+	// loremText = arel.Object.Model3D.createFromImage("loremText", "resources/lorem_text.png");
+	// loremText.setVisibility(false);
+	// loremText.setCoordinateSystemID(1);
+	// loremText.setScale(new arel.Vector3D(10.0,10.0,10.0));
+	// loremText.setTranslation(setPosition(loremText, 500, 0, 20));
+	// arel.Scene.addObject(loremText);
+	// ***************************************************************************************
+	// getting objects
+	// artistProfileButton = arel.Scene.getObject("artist_profile");	
+	// informationButton = arel.Scene.getObject("information");	
+	// closeButton = arel.Scene.getObject("close");		
+	// galleryButton = arel.Scene.grtObject('galleryButton');		
+	// textItem = arel.Scene.getObject("textItem");							
+	// ***************************************************************************************
+
+	artistProfileButton = arel.Object.Model3D.createFromImage("artistProfileButton", "resources/Buttons/artist.png");
+	artistProfileButton.setVisibility(true);
+	artistProfileButton.setCoordinateSystemID(1);
+	artistProfileButton.setScale(new arel.Vector3D(1.0,1.0,1.0));
+	artistProfileButton.setTranslation(setPosition(artistProfileButton, 0, 0, 20));
+	arel.Scene.addObject(artistProfileButton);
+
+	textItem = arel.Object.Model3D.createFromImage("textItem", "resources/Text/lorem_text.png");
+	textItem.setVisibility(false);
+	textItem.setCoordinateSystemID(1);
+	textItem.setScale(new arel.Vector3D(10.0,10.0,10.0));
+	textItem.setTranslation(setPosition(textItem, 0, 0, 0));
+	arel.Scene.addObject(textItem);
+
+	informationButton = arel.Object.Model3D.createFromImage("informationButton", "resources/Buttons/information.png");
+	informationButton.setVisibility(true);
+	informationButton.setCoordinateSystemID(1);
+	informationButton.setScale(new arel.Vector3D(1.0,1.0,1.0));
+	informationButton.setTranslation(setPosition(informationButton, 100, 0, 0));
+	arel.Scene.addObject(informationButton);
+
+	closeButton = arel.Object.Model3D.createFromImage("closeButton", "resources/Buttons/close.png");
+	closeButton.setVisibility(false);
+	closeButton.setCoordinateSystemID(1);
+	closeButton.setScale(new arel.Vector3D(1.0,1.0,1.0));
+	closeButton.setTranslation(setPosition(closeButton, 150, 0, 20));
+	arel.Scene.addObject(closeButton);
+
+	galleryButton = arel.Object.Model3D.createFromImage("galleryButton", "resources/Buttons/gallery.png");
+	galleryButton.setVisibility(true);
+	galleryButton.setCoordinateSystemID(1);
+	galleryButton.setScale(new arel.Vector3D(1.0,1.0,1.0));
+	galleryButton.setTranslation(setPosition(galleryButton, 200, 0, 20));
+	arel.Scene.addObject(galleryButton);
 };
--- Junaio/index.php
+++ Junaio/index.php
@@ -11,28 +11,28 @@ ArelXMLHelper::start(NULL, "arel/index.html", "resources/Tracking.zip");
 
 // BUTTONS **********************************************************************************
 // CLOSE
-$oObject = ArelXMLHelper::createGlueModel3D(
-	"close",															//ID
-	NULL,																//Path to model
-	"resources/Buttons/close.png",										//Path to texture
-	array(500,0,5),													//translation
-	array(10.0,10.0,10.0,),												//scale
-	new ArelRotation(ArelRotation::ROTATION_EULERDEG, array(0,0,0)),	//rotation
-	1																	//CoordinateSystemID
-);
-$oObject->setVisibility(true);
-ArelXMLHelper::outputObject($oObject);
+// $oObject = ArelXMLHelper::createGlueModel3D(
+// 	"close",															//ID
+// 	NULL,																//Path to model
+// 	"resources/Buttons/close.png",										//Path to texture
+// 	array(500,0,5),													//translation
+// 	array(10.0,10.0,10.0,),												//scale
+// 	new ArelRotation(ArelRotation::ROTATION_EULERDEG, array(0,0,0)),	//rotation
+// 	1																	//CoordinateSystemID
+// );
+// $oObject->setVisibility(true);
+// ArelXMLHelper::outputObject($oObject);
 // GALLERY
-$oObject = ArelXMLHelper::createGlueModel3D(
-	"gallery",																//ID
-	NULL,																//Path to model
-	"resources/Buttons/gallery.png",											//Path to texture
-	array(100,0,0),													//translation
-	array(1.0,1.0,1.0,),												//scale
-	new ArelRotation(ArelRotation::ROTATION_EULERDEG, array(0,0,0)),	//rotation
-	1																	//CoordinateSystemID
-);
-ArelXMLHelper::outputObject($oObject);
+// $oObject = ArelXMLHelper::createGlueModel3D(
+// 	"gallery",																//ID
+// 	NULL,																//Path to model
+// 	"resources/Buttons/gallery.png",											//Path to texture
+// 	array(100,0,0),													//translation
+// 	array(1.0,1.0,1.0,),												//scale
+// 	new ArelRotation(ArelRotation::ROTATION_EULERDEG, array(0,0,0)),	//rotation
+// 	1																	//CoordinateSystemID
+// );
+// ArelXMLHelper::outputObject($oObject);
 // ARTIST PROFILE
 // $oObject = ArelXMLHelper::createGlueModel3D(
 // 	"artist_profile",													//ID
@@ -45,18 +45,31 @@ ArelXMLHelper::outputObject($oObject);
 // );
 // $oObject->setVisibility(false);
 // ArelXMLHelper::outputObject($oObject);
+
 // INFORMATION
-$oObject = ArelXMLHelper::createGlueModel3D(
-	"information",																//ID
-	NULL,																//Path to model
-	"resources/Buttons/information.png",											//Path to texture
-	array(200,0,0),													//translation
-	array(1.0,1.0,1.0,),												//scale
-	new ArelRotation(ArelRotation::ROTATION_EULERDEG, array(0,0,0)),	//rotation
-	1																	//CoordinateSystemID
-);
-$oObject->setVisibility(false);
-ArelXMLHelper::outputObject($oObject);
+// $oObject = ArelXMLHelper::createGlueModel3D(
+// 	"information",																//ID
+// 	NULL,																//Path to model
+// 	"resources/Buttons/information.png",											//Path to texture
+// 	array(200,0,0),													//translation
+// 	array(1.0,1.0,1.0,),												//scale
+// 	new ArelRotation(ArelRotation::ROTATION_EULERDEG, array(0,0,0)),	//rotation
+// 	1																	//CoordinateSystemID
+// );
+// $oObject->setVisibility(false);
+// ArelXMLHelper::outputObject($oObject);
+
+// $oObject = ArelXMLHelper::createGlueModel3D(
+// 	"textItem",															//ID
+// 	NULL,																//Path to model
+// 	"resources/Text/lorem_text.png",									//Path to texture
+// 	array(200,0,5),														//translation
+// 	array(10.0,10.0,10.0,),												//scale
+// 	new ArelRotation(ArelRotation::ROTATION_EULERDEG, array(0,0,0)),	//rotation
+// 	1																	//CoordinateSystemID
+// );
+// $oObject->setVisibility(true);
+// ArelXMLHelper::outputObject($oObject);
 // BUTTONS END *******************************************************************************
 
 ArelXMLHelper::end();

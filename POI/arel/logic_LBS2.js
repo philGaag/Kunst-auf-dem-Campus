8e368cc4702a7fbbb9f23fcb00dfb627e36d00ab
Philip Gaag <philipgaag@yahoo.com>
2015-03-30 10:40:54 +0200

    new POI channel repo to replace the old one

--- POI/arel/logic_LBS2.js
+++ POI/arel/logic_LBS2.js
@@ -16,9 +16,8 @@ function handleCustomPoiEvent(obj, type, param)
 		// $('#info').show();
 	}
 };
-function openHelp(){
-	arel.Media.openWebsite("http://philg.avior.uberspace.de/helpText/index.html");
-};
+
+
 function changeChannel(){
 	arel.Scene.switchChannel(382664);
 };
